0e78a4fa8b535180c1f2fd1f4c0dbb51bf2c0de3
Maven, Nazi, A88 = all the same guy <40110835+Abo5@users.noreply.github.com>
2025-04-21 04:39:59 +0300

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,14 @@
+# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    # اسمح فقط بطلبات المتصفّح القادمة من واجهة Next.js
+    origins 'http://127.0.0.1:3000', 'http://localhost:3000'
+
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :patch, :put, :delete, :options, :head],
-      expose: ['Authorization']
+             headers:     :any,
+             methods:     %i[get post patch put delete options head],
+             expose:      ['Authorization'], # يرسل الهيدر JWT للمتصفّح
+             credentials: true               # يسمح بتمرير الكوكيز مع الطلب
   end
 end
+
